ff6cea6f7a0ee49352e4936bfb9ac15f0e72c3ff
Shivam <shivamgundale005@gmail.com>
2024-04-21 18:58:44 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+
+
 
 function App() {
+
+ 
+ const form = useRef();
+  const [formData, setFormData] = useState({
+    Name: '',
+    Email: '',
+    Phone_no: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target; 
+    setFormData({ 
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:8080/', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    
+  };
+
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>goCosmic Registration Form </h1>
+        <form ref={form} onSubmit={handleSubmit} >
+          <label>
+            <h2>Name:</h2>
+            <input type="text" name="Name" value={formData.Name} onChange={handleChange} />
+          </label>
+          <br />
+
+          <label>
+            <h2>Email:</h2>
+            <textarea name="Email" value={formData.Email} onChange={handleChange} />
+          </label>
+          <br />
+
+          <label>
+            <h2>Phone No:</h2>
+            <input type="text" name="Phone_no" value={formData.Phone_no} onChange={handleChange} />
+          </label>
+          <br />
+
+          <button type="submit" >Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
